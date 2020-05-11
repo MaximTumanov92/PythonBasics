b30519390c74e2833f4cc4a9d574176df5c6b152
MaximTumanov92 <64500090+MaximTumanov92@users.noreply.github.com>
2020-05-11 23:26:12 +0300

    Delete Task3.1.py

--- Task3.1.py
+++
@@ -1,9 +0,0 @@
-def my_fun(n_1, n_2):
-    try:
-        n_3 = n_1 / n_2
-        return n_3
-    except ZeroDivisionError:
-        print("Digit must differ from Zero")
-
-
-print(f"Result is {my_fun(int(input('Enter a digit: ')), int(input('Enter a digit: ')))}")
