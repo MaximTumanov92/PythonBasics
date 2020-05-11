61b78af3a1c2ddda7741f681bab473bac6725c54
MaximTumanov92 <64500090+MaximTumanov92@users.noreply.github.com>
2020-05-11 23:26:41 +0300

    Delete Task3.3.py

--- Task3.3.py
+++
@@ -1,9 +0,0 @@
-def my_fun(n_1, n_2, n_3):
-    try:
-        result = max(int(n_1) + int(n_2), int(n_2) + int(n_3), int(n_1) + int(n_3))
-        print(result)
-    except ValueError:
-        print("Enter digits only")
-
-
-my_fun(input("Enter first digit :"), input("Enter second digit :"), input("Enter third digit :"))
